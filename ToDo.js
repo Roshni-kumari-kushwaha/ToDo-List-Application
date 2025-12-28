7d96375644b026fc9fbbd9f65cf6a309714f8576
Roshni kushwaha <roshnikumarikushwaha2006@gmail.com>
2025-12-28 14:22:34 +0530

    Now added Backend and array to store tasks

--- ToDo.js
+++ ToDo.js
@@ -1,7 +1,15 @@
 const API_URL =  "http://localhost:3000/tasks";
 ;
 window.onload = fetchTasks();
-
+let currentEditId = null;
+const input = document.getElementById("taskInput");
+input.addEventListener("keydown",
+    function(e){
+        if(e.key === "Enter"){
+            addTask();
+        }
+    }
+)
 function fetchTasks() {
     fetch(API_URL)
         .then(res => res.json())
@@ -18,9 +26,9 @@ function fetchTasks() {
                     </span>
                
                 <div>
-                     <button class="btn-complete">✔️</button>
-                     <button class="btn-edit">✏️</button>
-                     <button class="btn-delete">❌</button>
+                     <button class="btn-complete" onclick="toggleTask(${task.id})">✔️</button>
+                     <button class="btn-edit" onclick="editTask(${task.id},'${task.title}')">✏️</button>
+                     <button class="btn-delete" onclick=" deleteTask(${task.id})">❌</button>
                 </div>
                  `;
                
@@ -33,13 +41,15 @@ function fetchTasks() {
 function addTask(){
     const input = document.getElementById("taskInput");
     const title = input.value.trim();
+
+   
     console.log(title);
 
     if(!title){
-        alert("Task cannot be empty");
-        return;
+        showMessage("Task cannot be empty");
+        return
     }
-
+   
     fetch(API_URL,{
         method:"POST",
         headers:{"Content-Type":"application/json"},
@@ -51,31 +61,59 @@ function addTask(){
 }
 
 function deleteTask(id){
-    fetch(${API_URL}/${id},
+    fetch(`${API_URL}/${id}`,
            {method:"DELETE"})
            .then(fetchTasks);
 }
 
 function toggleTask(id){
-    fetch(${API_URL}/${id},
-        {method:"DELETE"})
+    fetch(`${API_URL}/${id}`,
+        {method:"PATCH"})
         .then(fetchTasks);
 }
 
 function editTask(id,oldTitle){
-    const newTitle = prompt("Edit task:",oldTitle);
+    currentEditId = id;
 
-    if(!newTitle || newTitle.trim() === ""){
-        alert("Task cannot be empty");
-        return;
+    document.getElementById("editInput").value = oldTitle;
+    document.getElementById("editModal").classList.remove("hidden");
+
+}
+
+function closeModal(){
+    document.getElementById("editModal").classList.add("hidden");
+    currentEditId = null;
+}
+
+function saveEdit(){
+    const newTitle = document.getElementById("editInput").value.trim();
+    
+    if(newTitle === ""){
+        showMessage("Task cannot be empty");
+        return
     }
-
-    fetch(${API_URL}/${id},
+   fetch(`${API_URL}/${currentEditId}`,
         {
             method:"PUT",
             headers:{
                 "Content-Type":"application/json"},
             body:JSON.stringify({title:newTitle})
-        }).then(fetchTasks);
+        }).then(res =>{
+             if(res.ok){
+                fetchTasks();
+                closeModal();
+             }
+        });
+       
+}
+
+function showMessage(msg){
+    const toast = document.getElementById("toast");
+    toast.innerText = msg;
+    toast.classList.remove("hidden");
+
+    setTimeout(()=>{
+        toast.classList.add("hidden");
+    },2000);
 }
 
